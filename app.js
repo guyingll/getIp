39b80da3835bd1deb05bde1b6c3bb643168f4eb3
yanpeng <yanpeng@pset.suntec.net>
2014-11-04 09:29:13 +0800

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+var app = express();
+app.set('port', process.env.PORT || 8888);
+app.get('/', function(req, res) {
+    var ip=req.connection.remoteAddress,ips=[],data={};
+    data.ip=ip;
+    if(ip){
+        ips=ip.split(".");
+    }
+    if(ips.length==4){
+        if(ips[2]!=180){
+           data.city="上海市";
+        }else{
+           data.city="沈阳市";
+        }
+    }
+    res.send(data);
+});
+
+var server = http.createServer(app);
+
+server.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
